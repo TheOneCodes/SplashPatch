e834511a262eba169c33d01b4c9be03861282375
TheOneCode <evan.marko@outlook.com>
2020-09-20 14:03:00 -0500

    Add project files.

---
+++ Progress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SplashPatch
+{
+    public partial class Progress : Form
+    {
+        //private string currentEvent;
+
+        public Progress()
+        {
+            InitializeComponent();
+        }
+
+        private void WaitASec(object sender, FormClosingEventArgs e)
+        {
+            System.Threading.Thread.Sleep(1000);
+        }
+    }
+}
---
+++ Splash.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SplashPatch
+{
+    public partial class Splash : Form
+    {
+        string words = "Open source software by TheOneCode" + Environment.NewLine + Environment.NewLine + "Artwork of András Arató. For more details and legal notices, go to the About SplashPatch screen." + Environment.NewLine + Environment.NewLine;
+        Color text = Color.FromArgb(255, 142, 142, 142);
+        Color background = Color.FromArgb(255, 44, 47, 51);
+
+        public Splash()
+        {
+            InitializeComponent();
+            SplashLabel.BackColor = background;
+            SplashLabel.ForeColor = text;
+            BackgroundImage = Properties.Resources.SPLASH;
+        }
+
+        public void ChangeWords(string trail)
+        {
+            SplashLabel.Text = words + trail;
+        }
+
+        private void textChange(object sender, EventArgs e)
+        {
+            ChangeWords(Text);
+        }
+
+        private void swerliesStart(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.WaitCursor;
+        }
+
+        private void swerliesEnd(object sender, EventArgs e)
+        {
+            Cursor.Current = Cursors.Default;
+        }
+    }
+}
